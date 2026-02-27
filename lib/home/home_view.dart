98383aac22dbbca5d346e203fb916dc5e90bac47
choijunyoung1 <cpcp127@naver.com>
2026-02-27 13:45:35 +0900

    바텀 네비게이션바 원복

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -99,110 +99,87 @@ class _HomeViewState extends ConsumerState<HomeView> {
               ],
             ],
           ),
-          // bottomNavigationBar: Container(
-          //   decoration: const BoxDecoration(
-          //     border: Border(
-          //       top: BorderSide(
-          //         color: Colors.grey, // 테두리 색
-          //         width: 1, // 두께
-          //       ),
-          //     ),
-          //   ),
-          //   child: Theme(
-          //     data: Theme.of(context).copyWith(
-          //       splashColor: Colors.transparent,
-          //       highlightColor: Colors.transparent,
-          //       splashFactory: NoSplash.splashFactory, // ⭐ 핵심
-          //     ),
-          //     child: BottomNavigationBar(
-          //       onTap: (index) {
-          //         setState(() {
-          //           pageIndex = index;
-          //         });
-          //       },
-          //       type: BottomNavigationBarType.fixed,
-          //
-          //       elevation: 0,
-          //       backgroundColor: Colors.white,
-          //       selectedItemColor: Colors.black,
-          //       unselectedItemColor: Colors.grey,
-          //       currentIndex: pageIndex,
-          //       items: const [
-          //         BottomNavigationBarItem(icon: Icon(Icons.feed), label: '피드'),
-          //         BottomNavigationBarItem(icon: Icon(Icons.home), label: '모음'),
-          //         BottomNavigationBarItem(
-          //           icon: Icon(Icons.chat_bubble),
-          //           label: '채팅',
-          //         ),
-          //         BottomNavigationBarItem(
-          //           icon: Icon(Icons.person),
-          //           label: '프로필',
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
-          // body: IndexedStack(
-          //   index: pageIndex,
-          //   children: [
-          //     FeedListView(),
-          //     MeetListView(),
-          //     ChatListView(),
-          //     ProfileView(
-          //       uid: FirebaseAuth.instance.currentUser!.uid,
-          //       fromHomeTab: true,
-          //     ),
-          //   ],
-          // ),
-          body: PersistentTabView(
-            controller: _tabCtrl,
-            tabs: _buildTabs(),
-            onTabChanged: (index) {
-              setState(() {
-                pageIndex = index;
-              });
-            },
-            // ✅ 네비 스타일은 원하는 Style로 바꿔도 됨
-            navBarBuilder: (navBarConfig) => Style1BottomNavBar(
-              navBarConfig: navBarConfig,
-              navBarDecoration: NavBarDecoration(color: AppColors.bgWhite),
+          bottomNavigationBar: Theme(
+            data: Theme.of(context).copyWith(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              splashFactory: NoSplash.splashFactory, // ⭐ 핵심
             ),
+            child: BottomNavigationBar(
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
 
-            // ✅ 가운데 추가 버튼
-            floatingActionButton: _CenterAddButton(
-              onTap: () => {
-                controller.showCreateActionSheet(
-                  context: context,
-                  onCreateFeed: () {
-                    Navigator.push(
-                      context,
-                      CupertinoPageRoute(
-                        fullscreenDialog: true,
-                        builder: (context) {
-                          return CreateFeedView();
-                        },
-                      ),
-                    );
-                  },
-                  onCreateMeeting: () {
-                    Navigator.push(
-                      context,
-                      CupertinoPageRoute(
-                        fullscreenDialog: true,
-                        builder: (context) {
-                          return MeetCreateStepperView();
-                        },
-                      ),
-                    );
-                  },
-                ),
+              onTap: (index) {
+                if (index == 2) {
+                  controller.showCreateActionSheet(
+                    context: context,
+                    onCreateFeed: () {
+                      Navigator.push(
+                        context,
+                        CupertinoPageRoute(
+                          fullscreenDialog: true,
+                          builder: (context) {
+                            return CreateFeedView();
+                          },
+                        ),
+                      );
+                    },
+                    onCreateMeeting: () {
+                      Navigator.push(
+                        context,
+                        CupertinoPageRoute(
+                          fullscreenDialog: true,
+                          builder: (context) {
+                            return MeetCreateStepperView();
+                          },
+                        ),
+                      );
+                    },
+                  );
+                } else {
+                  setState(() {
+                    if (index == 0 || index == 1) {
+                      pageIndex = index;
+                    } else {
+                      pageIndex = index - 1;
+                    }
+                  });
+                }
               },
-            ),
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.miniCenterDocked,
+              type: BottomNavigationBarType.fixed,
 
-            // (선택) 키보드 올라올 때 화면 밀림 제어
-            resizeToAvoidBottomInset: true,
+              elevation: 0,
+              backgroundColor: Colors.white,
+              selectedItemColor: AppColors.btnPrimary,
+              unselectedItemColor: Colors.grey,
+              currentIndex: pageIndex,
+              items: const [
+                BottomNavigationBarItem(icon: Icon(Icons.feed), label: ''),
+                BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.add_box_outlined),
+                  label: '',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.chat_bubble),
+                  label: '',
+                ),
+                BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+              ],
+            ),
+          ),
+
+          body: IndexedStack(
+            index: pageIndex,
+            children: [
+              FeedListView(),
+              MeetListView(),
+              ChatListView(),
+              ProfileView(
+                uid: FirebaseAuth.instance.currentUser!.uid,
+                fromHomeTab: true,
+              ),
+            ],
           ),
         );
       },
@@ -213,70 +190,5 @@ class _HomeViewState extends ConsumerState<HomeView> {
   void initState() {
     super.initState();
     _tabCtrl = PersistentTabController(initialIndex: 0);
-  }
-
-  List<PersistentTabConfig> _buildTabs() {
-    return [
-      PersistentTabConfig(
-        screen: const FeedListView(),
-        item: ItemConfig(
-          icon: const Icon(Icons.view_list_outlined),
-          title: '피드',
-          activeForegroundColor: AppColors.icPrimary,
-          inactiveForegroundColor: AppColors.icSecondary,
-        ),
-      ),
-      PersistentTabConfig(
-        screen: const MeetListView(),
-        item: ItemConfig(
-          icon: const Icon(Icons.groups_outlined),
-          title: '모임',
-          activeForegroundColor: AppColors.icPrimary,
-          inactiveForegroundColor: AppColors.icSecondary,
-        ),
-      ),
-      PersistentTabConfig(
-        screen: const ChatListView(),
-        item: ItemConfig(
-          icon: const Icon(Icons.chat_bubble_outline_rounded),
-          title: '채팅',
-          activeForegroundColor: AppColors.icPrimary,
-          inactiveForegroundColor: AppColors.icSecondary,
-        ),
-      ),
-      PersistentTabConfig(
-        screen: ProfileView(
-          uid: FirebaseAuth.instance.currentUser!.uid,
-          fromHomeTab: true,
-        ),
-        item: ItemConfig(
-          icon: const Icon(Icons.person_outline),
-          title: '프로필',
-          activeForegroundColor: AppColors.icPrimary,
-          inactiveForegroundColor: AppColors.icSecondary,
-        ),
-      ),
-    ];
-  }
-}
-
-class _CenterAddButton extends StatelessWidget {
-  const _CenterAddButton({required this.onTap});
-
-  final VoidCallback onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 48,
-      height: 48,
-      child: FloatingActionButton(
-        onPressed: onTap,
-        backgroundColor: AppColors.btnPrimary,
-        elevation: 2,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
-        child: const Icon(Icons.add, color: AppColors.white, size: 26),
-      ),
-    );
   }
 }
