8c6bb010547e3d633a4926d647af279daaaa050c
choijunyoung1 <cpcp127@jetserve.net>
2026-03-16 14:53:48 +0900

    ios 수정

--- lib/meet/meet_detail/meat_detail_view.dart
+++ lib/meet/meet_detail/meat_detail_view.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 
-import 'package:blur/blur.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
